eefd4e964a545cb241e0d6aae2ea351e69c98034
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-24 17:16:50 +0200

    use registry server for configuration

--- config/development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    domains : [
-        "test.com",
-        "127.0.0.1",
-        "localhost",
-        "localhost:8000",,
-        "rauschen.info"
-    ]
-};
--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 "use strict";
 
+var version = require("./package").version;
 var server = require("./server");
-var path   = require("path");
-var config = require("./config");
+var distributor = require("./lib/distributor");
+var path = require("path");
+var environment = process.env.NODE_ENV || "development";
+var seaport = {
+    host: process.argv[2],
+    port: process.argv[3]
+};
 
-if (config.environment === "development") {
+var seaport = require("seaport");
+var ports = seaport.connect(seaport.host, seaport.port);
+
+var Scuttlebutt = require("scuttlebutt/model");
+var client = require("rauschen-registry").client;
+var config = new Scuttlebutt();
+
+client(seaport.host, seaport.port, config.createStream());
+
+config.on("data", function () {
+    var domainRestrictor = require(
+        path.join(__dirname, "lib", "domain-restrictor")
+    )(config.get("data").domains);
+
+    server.use(domainRestrictor);
+
+    distributor(server, ports.register("distributor@" + version));
+
+    server.listen(ports.register("receiver@" + version));
+
+    if (process.send) {
+        process.send("receiver turned on");
+
+        process.on("exit", function () {
+            process.send("receiver shutting down");
+        });
+    }
+});
+
+if (environment === "development") {
     new require("events-counter")(server, "new-request");
-}
-
-var domainRestrictor = require(
-    path.join(__dirname, "lib", "domain-restrictor")
-)(config.domains);
-
-server.use(domainRestrictor);
-
-require("./lib/distributor")(server);
-
-server.listen(config.receiver.port);
-
-if (process.send) {
-    process.send("receiver turned on");
-
-    process.on("exit", function () {
-        process.send("receiver shutting down");
-    });
 }
