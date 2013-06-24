d8caa1d907c2c5a26a40f4aeab506856831df04e
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-24 20:59:53 +0200

    fix registry and seaport connection

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 "use strict";
-
 var version = require("./package").version;
 var server = require("./server");
 var distributor = require("./lib/distributor");
-var path = require("path");
 var environment = process.env.NODE_ENV || "development";
 var seaport = {
     host: process.argv[2],
-    port: process.argv[3]
+    port: parseInt(process.argv[3], 10)
 };
 
-var seaport = require("seaport");
-var ports = seaport.connect(seaport.host, seaport.port);
-
-var Scuttlebutt = require("scuttlebutt/model");
 var client = require("rauschen-registry").client;
-var config = new Scuttlebutt();
+var config = client(seaport.host, seaport.port);
 
-client(seaport.host, seaport.port, config.createStream());
-
-config.on("data", function () {
-    var domainRestrictor = require(
-        path.join(__dirname, "lib", "domain-restrictor")
-    )(config.get("data").domains);
+config.on("update", function () {
+    var domainRestrictor = require("./lib/domain-restrictor")(config.get("data").domains);
 
     server.use(domainRestrictor);
 
+    var seaport = require("seaport");
+    var ports = seaport.connect(seaport.host, seaport.port);
+
     distributor(server, ports.register("distributor@" + version));
+    console.log("distributor started");
 
     server.listen(ports.register("receiver@" + version));
+    console.log("receiver started");
 
     if (process.send) {
         process.send("receiver turned on");
