cfba1919a74e0516d5e6ed3917f8588ccf1cb6ee
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-24 23:23:52 +0200

    do not overwrite seaport variable

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var version = require("./package").version;
 var server = require("./server");
 var distributor = require("./lib/distributor");
 var environment = process.env.NODE_ENV || "development";
-var seaport = {
+var seaportConf = {
     host: process.argv[2],
     port: parseInt(process.argv[3], 10)
 };
 
 var client = require("rauschen-registry").client;
-var config = client(seaport.host, seaport.port);
+var config = client(seaportConf.host, seaportConf.port);
 
 config.on("update", function () {
     var domainRestrictor = require("./lib/domain-restrictor")(config.get("data").domains);
@@ -17,7 +17,7 @@ config.on("update", function () {
     server.use(domainRestrictor);
 
     var seaport = require("seaport");
-    var ports = seaport.connect(seaport.host, seaport.port);
+    var ports = seaport.connect(seaportConf.host, seaportConf.port);
 
     distributor(server, ports.register("distributor@" + version));
     console.log("distributor started");
